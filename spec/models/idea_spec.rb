4c6a345e63d9be50754e03b4cda7cbe08fb9474b
MB Burch <marybethburch@gmail.com>
2015-11-30 15:02:00 -0700

    add idea model and model spec

---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Idea, type: :model do
+  let(:idea) { Idea.new(title: "First Idea", body: "This is my first idea.") }
+
+  it "should be valid" do
+    expect(idea).to be_valid
+  end
+
+  it "must have a title" do
+    idea.title = nil
+    expect(idea).to_not be_valid
+  end
+
+  it "must have a body" do
+    idea.body = nil
+    expect(idea).to_not be_valid
+  end
+end
