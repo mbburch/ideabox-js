c4db0a4434e2a04b4fb69c90c3991b98e75d40af
MB Burch <marybethburch@gmail.com>
2015-11-30 19:16:49 -0700

    add titleize to idea title

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,12 @@
 class Idea < ActiveRecord::Base
+  before_validation :titleize
+
   validates :title, presence: true
   validates :body, presence: true
 
   enum quality: %w(Swill Plausible Genius)
+
+  def titleize
+    self.title = title.to_s.titleize
+  end
 end
