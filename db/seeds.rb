8096be82088464bf9cd18cf495dc2cc03f5cfa84
MB Burch <marybethburch@gmail.com>
2015-11-30 19:11:53 -0700

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+  def initialize
+    generate_ideas
+  end
+
+  def generate_ideas
+    10.times do
+      idea = Idea.create(title: Faker::Hipster.word,
+                  body: Faker::Hipster.sentence,
+                  quality: [0, 1, 2].shuffle[rand])
+      puts "Idea #{idea.id} created!"
+    end
+  end
+
+
+end
+
+Seed.new
